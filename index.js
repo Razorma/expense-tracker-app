d803d2310401714ec16af640ce15741755ce927d
bheka <bheka.bj@gmail.com>
2023-11-29 09:58:28 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,62 @@
+import express  from "express";
+import { engine } from "express-handlebars";
+import bodyParser from "body-parser";
+import pgPromise from "pg-promise";
+import flash from "express-flash";
+import session from "express-session";
+import bcrypt from "bcrypt";
+import dotenv from 'dotenv'
+import cookieParser from "cookie-parser"
+dotenv.config();
+
+
+const app = express();
+const pgp = pgPromise();
+
+const connectionString = process.env.DATABASE_URL 
+
+const db = pgp({ connectionString});
+
+app.use(session({ 
+  secret: 'Razorma', 
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(flash());
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Parse URL-encoded and JSON request bodies
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+
+
+
+
+
+
+app.get('/',function(req,res,next){
+    req.render('home')
+})
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3014;
+
+app.listen(PORT, function(){
+  console.log(`App listening on localhost:${PORT}`);
+});
